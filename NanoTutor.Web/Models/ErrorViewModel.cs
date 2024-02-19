6cbf30dc10d1fceda366e4f89adacf7b7099e319
Creative_Nur <Creative_Nur@DESKTOP-CC8GO7A>
2024-02-20 02:23:14 +0600

    Add project files.

---
+++ NanoTutor.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NanoTutor.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
